561a4b594aa2302f77cdff87a0bb18b36ce974ef
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2019-12-20 11:29:30 +0800

    Update curl.php

--- curl.php
+++ curl.php
@@ -17,9 +17,7 @@ curl_setopt_array($curl, array(
     "Cache-Control: max-age=0",
     "Connection: keep-alive",
     "Content-Length: 0",
-    // "cookie: _ga=GA1.2.1046945946.1576548924; _gid=GA1.2.1708859978.1576548924; signIn.rememberMe=true; INDIVIDUAL_KEY=130469d0-5adb-4c00-9621-dd9493c4830a; JSESSIONID=E1036F90DBB139221F6A5AD04C5435FE; signInPanel__signInForm__username=BBinMobile; signInPanel__signInForm__password=nITbJKzP0p2p7sTSaxh79Q%3D%3D",
     "Host: dinbendon.net",
-    // "Postman-Token: 21d2a79d-8540-49c3-9927-a5e3bafd34a7,fea55f9c-149b-430d-96da-543a10837ac7",
     "Referer: https://dinbendon.net/do/login",
     "Sec-Fetch-Mode: navigate",
     "Sec-Fetch-Site: same-origin",
@@ -34,14 +32,11 @@ CURLOPT_COOKIE => "_ga=GA1.2.1046945946.1576548924; _gid=GA1.2.1708859978.157654
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 $html = curl_exec($curl);
 curl_close($curl);
-
-
 ////顯示爬出的含html格式的網頁
 //  echo $html;
 
 // // preg_match以陣列方式把這標籤包含的字串，只會抓出指定標籤第一筆
 //   $IsMatch= preg_match('/<span>(.*)<\/span>/', $html, $match);
-// //印出陣列
 //   if( $IsMatch ){
 //   print_r ($match[0]) . "\n." ;
 //   }
@@ -49,21 +44,17 @@ curl_close($curl);
 
 // //preg_match_all以陣列方式把這標籤包含的字串全部抓出來
 //   $IsMatch= preg_match_all('/><a href="(.*)"><span>(.*)<\/span></U', $html, $match);
-// //印出陣列
 //   if( $IsMatch ){
 //   print_r ($match[0]) . "\n." ;
 //   }
 
-  // 迴圈跑出全部標籤為<td ><a href="(.*)"><span>(.*)<\/span><\/a>的字串
-  //做正規表示法，要特別注意函數內的標籤顏色，要注意斜線跟反斜線的運用，若提示顏色沒變化，則可能是正規表示法操作錯誤
+  // foreach跑出全部標籤為<td ><a href="(.*)"><span>(.*)<\/span><\/a>的資料
+  //做正規表示法，要特別注意函數內過濾的標籤顏色，要注意斜線跟反斜線的運用，若提示顏色沒變化，則可能是正規表示法操作錯誤
   preg_match_all('/\<td ><a href="(.*)"><span>(.*)<\/span><\/a>/U', $html, $match);
-  //在php preg_match中預設是採用貪婪比對，太貪婪反而不符合需要，因此得採用「非貪婪比對」，只要在modifier 中加上"U"即可
   foreach ($match[0] as $key => $stores) {
     echo  strip_tags ($stores)."\n"."</br>";
   }
-　　　　date_default_timezone_set('Asia/Taipei');
-　　　echo date("Y-m-d H:i:s");
-
+　
    echo "<br>".'爬蟲結束時間:'.date("Y-m-d H:i:s",(time()+時差*3600)); 
 
  
