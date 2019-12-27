7631f30d08bbf27a61556d0672bc710c7dbc08ca
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2019-12-27 15:35:45 +0800

    Update curl-final.php

--- curl-final.php
+++ curl-final.php
@@ -23,7 +23,7 @@ $num=$match[1];
 preg_match_all('/[0-9]{0,}/',$num,$single);//利用array_sum函數把數值全部相加
 $code = array_sum($single[0]);// echo $code;//印出總和
 
-//抓出每次變更表單變更action網址串
+//抓出每次提交表單變更action後的網址串
 preg_match('/<form action="(.*)" id="signInPanel_signInForm" method="post">/U', $html, $ma);
 $a=$ma[1];
 // var_dump($a);
@@ -45,7 +45,7 @@ curl_exec($curl);
 curl_close($curl);
 
 
-//(3.)開始爬登入之後網頁的資料，並用正規表示法條件篩選出要的文字===========================================================================================
+//(3.)開始爬登入之後網頁的資料，並用正規表示法條件篩選出要的文字==========================================================================
 $data_url = "https://dinbendon.net/do"; //爬出登入後資料的網址
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $data_url);
@@ -61,7 +61,7 @@ curl_close($ch);
 
 echo "<hr>";
 
-//利用正規表示比對把要的新增店家欄位擷取出來
+//利用正規表示比對把要的新增店家欄位取出來
 preg_match_all('/\<td ><a href="(.*)"><span>(.*)<\/span><\/a>/U', $data, $match);
 //在php preg_match中預設是採用貪婪比對，太貪婪反而不符合需要，因此得採用「非貪婪比對」，只要在modifier 中加上"U"即可
 foreach ($match[0] as $key => $store) {
@@ -70,28 +70,69 @@ foreach ($match[0] as $key => $store) {
   echo "<br>".'爬蟲結束時間:'.date("Y-m-d H:i:s",(time()+8*3600))."<br>"; //(time()+8*3600)是改成台灣時間
   echo "<hr>";
 
-//(4.)爬出來的資料寫進資料庫中===========================================================================================
-// 建立MySQL的資料庫連接 
+//(4.)爬出來的資料寫進資料庫中===========================================================================================================
+//PDO連結資料庫寫法
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpasswd = '';
+$dbname = 'db';
+$dsn = "mysql:host=".$dbhost.";dbname=".$dbname;
 
-$link = mysqli_connect("localhost","root","","db")
-        or die("無法開啟MySQL資料庫連接!<br/>");
-echo "資料庫連接開始"."....."."<br>";
-echo "MySQL資料庫開啟成功!<br/>";
+try
+{
+    $conn = new PDO($dsn,$dbuser,$dbpasswd);
+    $conn->exec("SET CHARACTER SET utf8");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "連接資料庫成功"."<br>";
+}
+catch(PDOException $e)
+{
+    echo "資料庫連接失敗: ".$e->getMessage();
+}
 
-mysqli_query($link,'TRUNCATE TABLE stores');//清空資料表
+$conn->query("TRUNCATE TABLE stores");//清空資料表
+// mysqli_query($db,'TRUNCATE TABLE stores');
 
-$a = $match[0];//若直接讀取($store)變數值只會寫入最後一筆資料，需要用for迴圈
-for ($i=0;$i<8;$i++)//最新店家有8筆，故預設$i<8
+$a= $match[0];//若直接讀取($store)變數值只會寫入最後一筆資料，需要用for迴圈跑
+for ($i=0;$i<count($a);$i++)
 {
  $sqldata[] = "('".strip_tags($a[$i])."')";
 }
+$sql  = "INSERT INTO `stores` (`name`) VALUES(".implode ('),(', $sqldata).")";
 
-$sql = "INSERT INTO `stores`(`name`) VALUES(".implode ( $sqldata).")" 
-        or die("寫入失敗!");
-echo "SQL字串:".$sql ."<br>"."已成功寫入資料庫";
-mysqli_query($link, 'SET NAMES utf8'); 
-mysqli_query($link, $sql);
-mysqli_close($link);
+try
+{
+  $conn->query($sql);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "資料已成功寫入資料庫"."<br>";
+}
+catch(PDOException $e)
+{
+echo "資料寫入資料庫失敗: ".$e->getMessage();
+}
+$conn = NULL;
+
+// // mysqli方式寫法//建立MySQL的資料庫連接 
+// echo "資料庫連接開始"."....."."<br>";
+// $link = mysqli_connect("localhost","root","","db")
+//         or die("無法開啟MySQL資料庫連接!<br/>");
+// echo "MySQL資料庫開啟成功!<br/>";
+
+// mysqli_query($link,'TRUNCATE TABLE stores');//先清空資料表
+
+// $a = $match[0];//若直接讀取($store)變數值只會寫入最後一筆資料，需要用for迴圈跑
+// for ($i=0;$i<count($a);$i++)
+// {
+//  $sqldata[] = "('".strip_tags($a[$i])."')";
+// }
+
+// $sql = "INSERT INTO `stores`(`name`) VALUES(".implode ('),(', $sqldata).")" 
+//       or die("寫入失敗!");
+//     echo "SQL字串:".$sql ."<br>"."已成功寫入資料庫";
+//     //送出UTF8編碼的MySQL指令
+// mysqli_query($link, 'SET NAMES utf8'); 
+// mysqli_query($link, $sql);
+// mysqli_close($link);
 ?>
 
 
