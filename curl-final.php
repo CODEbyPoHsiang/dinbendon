692419ccc769818f1a06ef0f5810251a6209b877
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2019-12-27 16:10:40 +0800

    Update curl-final.php

--- curl-final.php
+++ curl-final.php
@@ -71,72 +71,9 @@ foreach ($match[0] as $key => $store) {
   echo "<hr>";
 
 //(4.)爬出來的資料寫進資料庫中===========================================================================================================
-//PDO連結資料庫寫法
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpasswd = '';
-$dbname = 'db';
-$dsn = "mysql:host=".$dbhost.";dbname=".$dbname;
 
-try
-{
-    $conn = new PDO($dsn,$dbuser,$dbpasswd);
-    $conn->exec("SET CHARACTER SET utf8");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "連接資料庫成功"."<br>";
-    $conn->query($sql);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "資料已成功寫入資料庫"."<br>";
-  echo "<br>".'資料寫入時間:'.date("Y-m-d H:i:s",(time()+8*3600))."<br>";  
-}
-catch(PDOException $e)
-{
-    echo "資料庫連接失敗: ".$e->getMessage();
-}
-
-$conn->query("TRUNCATE TABLE stores");//清空資料表
-// mysqli_query($db,'TRUNCATE TABLE stores');
-
-$a= $match[0];//若直接讀取($store)變數值只會寫入最後一筆資料，需要用for迴圈跑
-for ($i=0;$i<count($a);$i++)
-{
- $sqldata[] = "('".strip_tags($a[$i])."')";
-}
-$sql  = "INSERT INTO `stores` (`name`) VALUES(".implode ('),(', $sqldata).")";
-
-try
-{
-  $conn->query($sql);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "資料已成功寫入資料庫"."<br>";
-}
-catch(PDOException $e)
-{
-echo "資料寫入資料庫失敗: ".$e->getMessage();
-}
-$conn = NULL;
-
-// // mysqli方式寫法//建立MySQL的資料庫連接 
-// echo "資料庫連接開始"."....."."<br>";
-// $link = mysqli_connect("localhost","root","","db")
-//         or die("無法開啟MySQL資料庫連接!<br/>");
-// echo "MySQL資料庫開啟成功!<br/>";
-
-// mysqli_query($link,'TRUNCATE TABLE stores');//先清空資料表
-
-// $a = $match[0];//若直接讀取($store)變數值只會寫入最後一筆資料，需要用for迴圈跑
-// for ($i=0;$i<count($a);$i++)
-// {
-//  $sqldata[] = "('".strip_tags($a[$i])."')";
-// }
-
-// $sql = "INSERT INTO `stores`(`name`) VALUES(".implode ('),(', $sqldata).")" 
-//       or die("寫入失敗!");
-//     echo "SQL字串:".$sql ."<br>"."已成功寫入資料庫";
-//     //送出UTF8編碼的MySQL指令
-// mysqli_query($link, 'SET NAMES utf8'); 
-// mysqli_query($link, $sql);
-// mysqli_close($link);
+//連接資料庫
+  require_once './curl-db.php';
 ?>
 
 
